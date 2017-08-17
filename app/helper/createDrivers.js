bc9b0637e3eae5141eed05fb18182dbe9b2dbfce
Edvard Gzraryan <edvardgzraryan@gmail.com>
2017-08-17 14:31:32 +0300

    Added method to create test drivers.

---
+++ app/helper/createDrivers.js
@@ -0,0 +1,27 @@
+var Driver = require('../models/driver');
+
+var createDriver = function (count) {
+    var namePrefix = 'driver';
+    for (var i = 0; i < count; ++i) {
+        var driverName = namePrefix + i;
+        var driver = new Driver();
+        driver.username = driverName;
+        driver.reserved = false;
+        driver.location = [getRandomArbitrary(), getRandomArbitrary()];
+
+        driver.save(function (err) {
+            if (err) {
+                console.log('Error when creating drivers: ' + JSON.stringify(err));
+                return;
+            }
+            console.log('Driver with name=' + driverName + ' is successfuly create');
+        });
+    }
+};
+
+var getRandomArbitrary = function () {
+    var min = 35, max = 55;
+    return JSON.parse((Math.random() * (max - min) + min).toPrecision(5));
+};
+
+createDriver(100);
